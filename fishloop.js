e99f2e46c243eb4eaac25d8ad5ac316db1a2bfdf
Zaldana <rooben.zaldana@gmail.com>
2021-06-22 21:36:20 -0400

    working fish loop

--- fishloop.js
+++ fishloop.js
@@ -1,59 +1,27 @@
-
-// Prompt Sync =========================================================
-
-const prompt = require('prompt-sync')({ sigint: true });
-
-// Multiline Bug Fix ===================================================
-
-const multiLinePrompt = ask => {
-    const lines = ask.split(/\r?\n/);
-    const promptLine = lines.pop();
-    console.log(lines.join('\n'));
-    return prompt(promptLine);
-};
-
-//Random fish generator function ======================================
-
 //Fish adjective 1
 const adjOne = ["giant", "lazy", "snapping", "hairy", "iridescent", "sexy", "ornery", "spiny", "venomous", "classic"];
 
 //Fish adjective 2
 const adjTwo = ["squishy", "chunky", "sneaky", "suspicious", "sensitive", "greedy", "rabid", "angry", "stupid", "yummy"];
 
-//Fish Type
+//Type of Fish
 const fish = ["trout", "carp", "catfish", "loach", "bass", "tilapia", "salmon", "arowana", "shark", "guppy"];
 
-//Function
 function generateRandomFish(adjOne, adjTwo, fish) {
 
-    //Empty first adjective   
     let firstAdj = [];
-
-    //Empty first adjective
     let secndAdjs = [];
-
-    //Empty fish type
     let fishType = [];
-
-    //Random weight
     const weight = (Math.floor((Math.random() * 10)) + 1);
-
-    //Randon value and change to 2 decimal places
     const valueRnd = Math.round(Math.random() * 10);
     const value = valueRnd.toFixed(2);
 
-    //For loop for arrays
     for (i = 0; i < 1; i++) {
-
-        //Random generated first adjective
         firstAdj = adjOne[Math.floor((Math.random() * 9)) + 1];
-
-        //Random generated second adjective
         secndAdjs = adjTwo[Math.floor((Math.random() * 9)) + 1];
         fishType = fish[Math.floor((Math.random() * 9)) + 1];
     }
 
-    //Return random fish object
     return {
         fish: firstAdj + " " + secndAdjs + " " + fishType,
         weight: weight + "lbs",
